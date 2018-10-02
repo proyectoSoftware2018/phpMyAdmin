f077014ef3338548f0bac4bf544d88ab2326272b
proyectoSoftware2018 <43715815+proyectoSoftware2018@users.noreply.github.com>
2018-10-02 12:48:46 -0500

    mi propio phpmuyadmin cambio1

--- config.inc.php
+++ config.inc.php
@@ -28,8 +28,8 @@ $i++;
 /* Authentication type */
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
 /* Server parameters */
-$cfg['Servers'][$i]['host'] = 'mysql';
-$cfg['Servers'][$i]['compress'] = false;
+$cfg['Servers'][$i]['host'] = 'jws-app-mysql';
+$acfg['Servers'][$i]['compress'a] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = false;
 
 /**
